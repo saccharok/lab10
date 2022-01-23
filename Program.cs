af2e669cc9139f4cd5104cce9069e4b578bfefaf
saccharok <90149992+saccharok@users.noreply.github.com>
2022-01-24 02:27:27 +0700

    Довести до ума ;-)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab9CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
